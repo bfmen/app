ba270343de4000f3f47e6acadbde8c5f7994034d
smanx <497704568@qq.com>
2022-05-09 15:23:11 +0000

    Deploying to master from @ smanx/vapp@0fa638315b2f3323536ffcbd43f589561fcc65c4 🚀

--- vapp/swc.js
+++ vapp/swc.js
@@ -1,36 +1,79 @@
 const cacheName = 'cacheName'
 
-let includes = ['list', '.png']
-
-function isCache(event) {
-    let url = event.request.url
-    return includes.some(include => url.includes(include))
+function isCache1(event) {
+	let url = event.request.url
+	return ['img3.xjnewpic.com', '/vod/reqplay/'].some(include => url.includes(include))
 }
 
+function isCache2(event) {
+	let url = event.request.url
+	return ['6jwti3892pf605m004mn.lagoapps.com'].some(include => url.includes(include))
+}
+
+function isCache3(event) {
+	return false
+}
+
+function isCacheType(event) {
+	let url = event.request.url
+	if (isCache1(event)) return 1
+	if (isCache2(event)) return 2
+	if (isCache3(event)) return 3
+	return 0
+}
+
+
 self.addEventListener('fetch', function (event) {
-    console.log('fetch', event.request.url)
-    if (isCache(event)) {
-        event.respondWith(
-            caches.open(cacheName).then(function (cache) {
-                // console.log('fetch cacheName', cache)
-                return cache.match(event.request).then(function (response) {
-                    return response || fetch(event.request).then(function (response) {
-                        cache.put(event.request, response.clone());
-                        console.log('fetch put', response)
-                        return response;
-                    });
-                });
-            })
-        );
-    }
+	console.log('fetch', event.request.url)
+	if (isCache1(event)) {
+		event.respondWith(
+			caches.open(cacheName).then((cache) => {
+				return cache.match(event.request).then((cacheResponse) => {
+					return cacheResponse || fetch(event.request).then((networkResponse) => {
+						cache.put(event.request, networkResponse.clone())
+						return networkResponse
+					})
+				})
+			}))
+	} else if (isCache2(event)) {
+		event.respondWith(
+			caches.open(cacheName).then((cache) => {
+				return caches.match(event.request).then((cacheResponse) => {
+					const fetchPromise = fetch(event.request).then((networkResponnse) => {
+						cache.put(event.request, networkResponnse)
+						return networkResponnse
+					})
+					return cacheResponse || fetchPromise
+				})
+			})
+		)
+	} else if (isCache3(event)) {
+		event.respondWith(
+			caches.open(cacheName).then((cache) => {
+				return fetch(event.request).then((networkResponse) => {
+					cache.put(event.request, networkResponse.clone())
+					return networkResponse
+				}).cache(() => {
+					return cache.match(event.request)
+				})
+			})
+		)
+	} else {
+		caches.open(cacheName).then((cache) => {
+			cache.delete(event.request)
+		})
+	}
 });
 
 self.addEventListener('message', function (event) {
-    // if (event.data == 'skipWaiting') {
-    //     self.skipWaiting()
-    // }
+	// if (event.data == 'skipWaiting') {
+	//     self.skipWaiting()
+	// }
 });
 
 self.addEventListener('install', function (event) {
-    event.currentTarget.skipWaiting()
+	event.currentTarget.skipWaiting()
 });
+
+
+
