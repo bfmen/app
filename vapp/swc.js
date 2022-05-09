31dcf46476ea23923dd127d4ddfb8beee4cd78de
smanx <497704568@qq.com>
2022-05-09 08:51:06 +0000

    Deploying to master from @ smanx/vapp@b62b55151642ab9d042dd718c1e80ada445e2f30 🚀

---
+++ vapp/swc.js
@@ -0,0 +1,35 @@
+const cacheName = 'cacheName'
+
+let includes = ['list', '.png']
+
+function isCache(event) {
+    let url = event.request.url
+    return includes.some(include => url.includes(include))
+}
+
+self.addEventListener('fetch', function (event) {
+    if (isCache(event)) {
+        event.respondWith(
+            caches.open(cacheName).then(function (cache) {
+                // console.log('fetch cacheName', cache)
+                return cache.match(event.request).then(function (response) {
+                    return response || fetch(event.request).then(function (response) {
+                        cache.put(event.request, response.clone());
+                        console.log('fetch put', response)
+                        return response;
+                    });
+                });
+            })
+        );
+    }
+});
+
+self.addEventListener('message', function (event) {
+    // if (event.data == 'skipWaiting') {
+    //     self.skipWaiting()
+    // }
+});
+
+self.addEventListener('install', function (event) {
+    event.currentTarget.skipWaiting()
+});
