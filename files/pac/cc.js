51b705cb3670fa6c707367b84a692c0bd7fed1dd
cc <email>
2022-06-26 21:41:11 +0800

    save

--- files/pac/cc.js
+++ files/pac/cc.js
@@ -3,8 +3,7 @@
 // HK - 香港原生【VIP2】 
 var proxy="HTTPS hk.cdn.nttsoftgroup.com:54300;HTTPS uu0x2VeWegUg.harmonycloudcache.com:22443;HTTPS node0x2VeWegUg.api.steamgamepowered.com:19443; DIRECT";
 // JP - 日本东京 【VIP2】
-// var proxy="HTTPS img0x2VeWegUg.virginatlanticgroup.com:27443;HTTPS jp.cdn.nttsoftgroup.com:52100; DIRECT";
-//  var proxy="HTTPS img0x2VeWegUg.virginatlanticgroup.com:27443; DIRECT";
+//  var proxy="HTTPS img0x2VeWegUg.virginatlanticgroup.com:27443;HTTPS jp.cdn.nttsoftgroup.com:52100; DIRECT";
 // US - 美国硅谷【VIP2】
 //  var proxy="HTTPS www.macyscdn.com:53100;HTTPS www.macyscdn.com:33100; DIRECT";
 // RU - 俄罗斯西伯利亚【VIP2】
