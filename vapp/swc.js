50db2a65564b7a60ecdb602408654c0b9a37769e
smanx <497704568@qq.com>
2022-07-29 09:42:49 +0000

    Deploying to master from @ smanx/vapp@2e45dd6dc5267e4cf32fb5f52de88a8db615a962 🚀

--- vapp/swc.js
+++ vapp/swc.js
@@ -2,7 +2,12 @@ const cacheName = 'cacheName'
 
 function isCache1(event) {
 	let url = event.request.url
-	return ['.jpg', 'the-awesome-smanx-site.netlify.app/coverpic'].some(include => url.includes(include))
+	return [
+		'.jpg', 
+		'the-awesome-smanx-site.netlify.app/coverpic',
+		'/pornstars/img',
+		'/pornstars/webm'
+	].some(include => url.includes(include))
 }
 
 function isCache2(event) {
