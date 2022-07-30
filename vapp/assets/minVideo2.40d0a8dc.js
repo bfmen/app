ef24d04c2391610539058e8170df1acfcfbd31e0
smanx <497704568@qq.com>
2022-07-30 01:10:53 +0000

    Deploying to master from @ smanx/vapp@54ce35866c7e3237c132e4c162ec2110f8865ad8 🚀

--- vapp/assets/minVideo2.40d0a8dc.js
+++ vapp/assets/minVideo2.40d0a8dc.js
@@ -1,4 +1,4 @@
-import{c as _}from"./config.73be10ef.js";import{n as zt}from"./index.091d2229.js";/*!
+import{c as _}from"./config.73be10ef.js";import{n as zt}from"./index.15f5f5b8.js";/*!
  * better-scroll / better-scroll
  * (c) 2016-2021 ustbhuangyi
  * Released under the MIT License.
