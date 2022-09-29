473bfaf1f79f3897bb7524c2d92fefe2c13d65e8
cc <email>
2022-09-30 06:41:02 +0800

    save

--- other/nine/libs/utils.js
+++ other/nine/libs/utils.js
@@ -83,6 +83,7 @@ let utils = {
           try {
             resolve(JSON.parse(data ? unzip(data) : '{}'))
           } catch (error) {
+            console.error('getDataSource error', error)
             resolve({})
           }
         });
@@ -90,6 +91,7 @@ let utils = {
     },
     saveDataSource: async (dataSourceTxtName, dataSource) => {
       let dataSourceStr = zip(dataSource)
+      // dataSourceStr = utils.trans.string2hex(dataSourceStr)
       // dataSourceStr =  JSON.stringify(dataSource)
       return await new Promise((resolve, reject) => {
         fs.writeFile(dataSourceTxtName, dataSourceStr, (err) => {
@@ -102,6 +104,23 @@ let utils = {
         })
       })
     },
+  },
+  trans: {
+    string2base64: function (str) {
+      return new Buffer(str).toString('base64')
+    },
+    base642string: function (base6) {
+      return new Buffer(base6, 'base64').toString()
+    },
+    string2hex: function (str) {
+      return new Buffer(str, 'utf8').toString('hex')
+    },
+    hex2string: function (hex) {
+      return new Buffer(hex, 'hex').toString('utf8')
+    },
+    isHexStr: function (str) {
+      return /^[A-Fa-f0-9]{1,}$/.test(str)
+    }
   }
 }
 
@@ -177,6 +196,7 @@ function stringToUint8Array(str) {
   var tmpUint8Array = new Uint8Array(arr);
   return tmpUint8Array
 }
+
 
 export default utils
 // module.exports = utils
