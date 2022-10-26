b4f669e572b76118409e25c3f3890a6decd67b07
smanx <497704568@qq.com>
2022-10-26 06:46:01 +0000

    Deploying to master from @ smanx/vapp3@98115c2e8fdb3473a0d2f93d904abb0782f3ebd1 🚀

---
+++ vapp3/assets/index.0c606dc9.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as n}from"./index.d7b1379d.2022_10_26_06_45_50.js";const o={};function r(t,s){return c(),n("div",null,"demo")}const a=e(o,[["render",r]]);export{a as default};
