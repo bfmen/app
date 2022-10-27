3c8b4cba19a010889a264246ee0a31fe17f24279
smanx <497704568@qq.com>
2022-10-27 06:59:43 +0000

    Deploying to master from @ smanx/vapp3@8dd4eaa46ba5d94cd51958f12b312a12fe3f6002 🚀

--- vapp3/assets/star.d005769a.js
+++
@@ -1,1 +0,0 @@
-import r from"./axios.226d1664.js";import"./index.b805869b.js";import{_ as a,o as e,c as o}from"./index.bdd1db7f.2022_10_27_04_28_54.js";import"./axios.91e25212.js";import"./config.f31e076d.js";const s={async created(){let t=await r();format(t.data)}};function c(t,n,p,i,m,_){return e(),o("div",null,"star")}const x=a(s,[["render",c]]);export{x as default};
